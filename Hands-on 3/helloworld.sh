57b75389afa38c96740462dbc3ad59aec6bb5987
Andres Vargas <55038007+andres-var@users.noreply.github.com>
2024-02-28 19:00:56 -0600

    Create helloworld.sh

---
+++ Hands-on 3/helloworld.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Imprimir en pantalla "Hello World"
+echo "Hello World!"
+
+# Listar el contenido del directorio actual
+ls -l
+
+# Crear un directorio llamado "Test"
+mkdir Test
+
+# Cambiarse al directorio "Test"
+cd Test
+
+# Listar el contenido del directorio "Test"
+ls -l
+
+# Salir del directorio "Test"
+cd ..
