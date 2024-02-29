c55cf37c8c47be2f7bc96de7a85625913e810569
Andres Vargas <55038007+andres-var@users.noreply.github.com>
2024-02-28 18:56:00 -0600

    Create helloworld.bat

---
+++ Hands-on 2/helloworld.bat
@@ -0,0 +1,22 @@
+@echo off
+
+rem Imprimir en pantalla "Hello World"
+echo Hello World!
+
+rem Listar el contenido del directorio actual
+dir
+
+rem Crear un subdirectorio (folder) llamado Test
+md Test
+
+rem Cambiarse al subdirectorio Test
+cd Test
+
+rem Listar el contenido del subdirectorio Test
+dir
+
+rem Salir del subdirectorio Test
+cd ..
+
+rem (Opcional) Pausa para que el usuario vea el resultado
+pause
